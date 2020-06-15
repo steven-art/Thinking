96b4ae5adcaccfd4deee983d85e2f5e8db9785b0
steven <tanguysteven15@gmail.com>
2020-06-15 16:51:24 +0200

    creation of the first page of this app and start the page of menu

--- app/src/main/java/com/tanguysteven/thinking/MainActivity.java
+++ app/src/main/java/com/tanguysteven/thinking/MainActivity.java
@@ -1,8 +1,10 @@
 package com.tanguysteven.thinking;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    Button ButtonIdea = (Button) findViewById(R.id.activity_main_idea_btn);
+       ButtonIdea.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+// Click event trigger here
+        }
+    });
 }
